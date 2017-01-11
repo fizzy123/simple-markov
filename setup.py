0defcafd15d8e2a55c9d6f8beb40200be6b5e8a5
Nobel Yoo <nobel@bastille.io>
2017-01-11 11:22:03 -0800

    first commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name="simple_markov",
+    version='1.0',
+    author="Nobel Yoo",
+    author_email="nobr@nobr.me",
+    url="https://github.com/fizzy123/simple-markov",
+    description="A simple markov module",
+    keywords="nlp markov",
+    packages=['simple_markov']
+)
---
+++ simple_markov/markov.py
@@ -0,0 +1,32 @@
+""" This module contains a class for generating markov strings. """
+import random
+import re
+
+class Markov():
+    """ This class remembers a corpus of text and generates markov strings. """
+
+    def __init__(self, text):
+        self.text = text
+
+    def generate(self):
+        """ This method generates markov strings. """
+        blobs = [blob for blob in self.text.split('\n') if blob]
+        word = random.choice(blobs).split(' ')[0]
+        sentence = word
+        still_chuggin = True
+        while still_chuggin:
+            matches = [(m.start(0), m.end(0)) for m in re.finditer(word + " ", self.text)]
+            if matches:
+                match = random.choice(matches)
+                word = self.text[match[0]:].split(' ')[1]
+                if "\n" in word:
+                    still_chuggin = False
+                sentence = sentence + " " + word.split('\n')[0]
+            else:
+                still_chuggin = False
+
+        print(sentence)
+
+    def add_text(self, text):
+        """ This method is for when you wanna add text and don't wanna reinstanciate """
+        self.text = self.text + "\n" + text
